f58d9a1c0eb45da012a9f55860617317993edcc8
Leela Venkata Prasad <lvprasad.dev@gmail.com>
2026-04-02 12:42:18 +0530

    Remove old Stripe checkout page code

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react"
 import CheckoutPageClient from "./CheckoutPageClient"
 
 export default function CheckoutPage() {
-  return (
-    <Suspense fallback={null}>
-      <CheckoutPageClient />
-    </Suspense>
-  )
+  return <CheckoutPageClient />
 }
